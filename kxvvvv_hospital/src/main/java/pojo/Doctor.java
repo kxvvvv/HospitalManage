69c2925c5cca7eac6fbc8d2c8653b11a8bc643b8
kxvvvv <773670006@qq.com>
2024-07-07 10:41:34 +0800

    测试：首次提交，增加pojo.Doctor及Doctor基础属性

---
+++ kxvvvv_hospital/src/main/java/pojo/Doctor.java
@@ -0,0 +1,15 @@
+package pojo;
+
+public class Doctor {
+    int docId;
+    String name;
+    String docPwd;//密码
+    String docAcc;//账号
+    int role;
+    int sectionId;//所属科室id
+    String phone;
+    String major;//主治专业
+    String job;//职称
+    String degree;//学历
+    int regFee;//挂号费用
+}
