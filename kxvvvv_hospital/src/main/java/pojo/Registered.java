a6f4b92d1fb6a796e6a155f84da6109e12256620
Hua <1614738757@qq.com>
2024-07-07 17:04:12 +0800

    更新pojo中的registered和medRecord

--- kxvvvv_hospital/src/main/java/pojo/Registered.java
+++ kxvvvv_hospital/src/main/java/pojo/Registered.java
@@ -1,5 +1,33 @@
 package pojo;
 
+import java.time.LocalDateTime;
+
 //挂号
 public class Registered {
+    // 挂号ID
+    private Integer registrationId;
+
+    // 医生ID
+    private Integer docId;
+
+    // 病人ID
+    private Integer patientId;
+
+    // 所需的费用
+    private String fee;
+
+    // 挂号状态，例如“已挂号”0、“已完成”1、“已取消”2等
+    private Integer status;
+
+    // 挂号发生的日期和时间
+    private LocalDateTime registrationTime;
+
+    // 病人预约的就诊日期
+    private LocalDateTime appointmentDate;
+
+    // 当前所预约时段(上下午)已有人数，20为满
+    private Integer peopleBefore;
+
+    // 预约时段，0为上午，1为下午
+    private Integer timeframe;
 }
--- kxvvvv_hospital/src/main/java/pojo/medRecord.java
+++ kxvvvv_hospital/src/main/java/pojo/medRecord.java
@@ -1,5 +1,33 @@
 package pojo;
 
+import java.time.LocalDateTime;
+
 //病例
 public class medRecord {
+    // 病历ID
+    private Integer medicalRecordId;
+
+    // 病人ID
+    private Integer patientId;
+
+    // 医生ID
+    private Integer doctorId;
+
+    // 性别
+    private String gender;
+
+    // 年龄
+    private String age;
+
+    // 治疗方案
+    private String treatmentPlan;
+
+    // 创建时间
+    private LocalDateTime createTime;
+
+    // 药品ID
+    private Integer drugId;
+
+    // 该病例对应的挂号ID
+    private Integer regId;
 }
