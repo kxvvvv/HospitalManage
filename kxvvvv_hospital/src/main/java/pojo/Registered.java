742cb2f2f27eace8b995a74e93b0f9623c46dccf
Hua <1614738757@qq.com>
2024-07-07 18:08:50 +0800

    去掉private

--- kxvvvv_hospital/src/main/java/pojo/Registered.java
+++ kxvvvv_hospital/src/main/java/pojo/Registered.java
@@ -5,29 +5,29 @@ import java.time.LocalDateTime;
 //挂号
 public class Registered {
     // 挂号ID
-    private Integer registrationId;
+    Integer registrationId;
 
     // 医生ID
-    private Integer docId;
+    Integer docId;
 
     // 病人ID
-    private Integer patientId;
+    Integer patientId;
 
     // 所需的费用
-    private String fee;
+    String fee;
 
     // 挂号状态，例如“已挂号”0、“已完成”1、“已取消”2等
-    private Integer status;
+    Integer status;
 
     // 挂号发生的日期和时间
-    private LocalDateTime registrationTime;
+    LocalDateTime registrationTime;
 
     // 病人预约的就诊日期
-    private LocalDateTime appointmentDate;
+    LocalDateTime appointmentDate;
 
     // 当前所预约时段(上下午)已有人数，20为满
-    private Integer peopleBefore;
+    Integer peopleBefore;
 
     // 预约时段，0为上午，1为下午
-    private Integer timeframe;
+    Integer timeframe;
 }
--- kxvvvv_hospital/src/main/java/pojo/medRecord.java
+++ kxvvvv_hospital/src/main/java/pojo/medRecord.java
@@ -5,29 +5,29 @@ import java.time.LocalDateTime;
 //病例
 public class medRecord {
     // 病历ID
-    private Integer medicalRecordId;
+    Integer medicalRecordId;
 
     // 病人ID
-    private Integer patientId;
+    Integer patientId;
 
     // 医生ID
-    private Integer doctorId;
+    Integer doctorId;
 
     // 性别
-    private String gender;
+    String gender;
 
     // 年龄
-    private String age;
+    String age;
 
     // 治疗方案
-    private String treatmentPlan;
+    String treatmentPlan;
 
     // 创建时间
-    private LocalDateTime createTime;
+    LocalDateTime createTime;
 
     // 药品ID
-    private Integer drugId;
+    Integer drugId;
 
     // 该病例对应的挂号ID
-    private Integer regId;
+    Integer regId;
 }
